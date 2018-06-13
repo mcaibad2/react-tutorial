49532956c7e0d2e908181fbaad84cafaa40481a1
Andreas Daskalopoulos <Andreas.Daskalopoulos@citrix.com>
2018-06-13 12:13:36 +0300

    Added gitignore

--- src/index.js
+++ src/index.js
@@ -7,14 +7,62 @@ class FriendsContainer extends React.Component {
 
     this.state = {
       name: 'Tyler McGinnis',
-      friends: ['Jake Lingwall', 'Sarah Drasner', 'Merrick Christensen']
+      friends: [
+        'Jake Lingwall',
+        'Sarah Drasner',
+        'Merrick Christensen'
+      ],
     }
+
+    this.addFriend = this.addFriend.bind(this)
+  }
+  addFriend(friend) {
+    this.setState((state) => ({
+      friends: state.friends.concat([friend])
+    }))
   }
   render() {
     return (
       <div>
         <h3> Name: {this.state.name} </h3>
+        <AddFriend addNew={this.addFriend} />
         <ShowList names={this.state.friends} />
+      </div>
+    )
+  }
+}
+
+class AddFriend extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      newFriend: ''
+    }
+
+    this.updateNewFriend = this.updateNewFriend.bind(this)
+    this.handleAddNew = this.handleAddNew.bind(this)
+  }
+  updateNewFriend(e) {
+    this.setState({
+      newFriend: e.target.value
+    })
+  }
+  handleAddNew() {
+    this.props.addNew(this.state.newFriend)
+    this.setState({
+      newFriend: ''
+    })
+  }
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.newFriend}
+          onChange={this.updateNewFriend}
+        />
+        <button onClick={this.handleAddNew}> Add Friend </button>
       </div>
     )
   }
@@ -26,7 +74,9 @@ class ShowList extends React.Component {
       <div>
         <h3> Friends </h3>
         <ul>
-          {this.props.names.map((friend) => <li>{friend}</li>)}
+          {this.props.names.map((friend) => {
+            return <li> {friend} </li>
+          })}
         </ul>
       </div>
     )
